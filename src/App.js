a211685813be008b49d8b170215f10c143112ff4
kimji9663 <rlawlgp9663@naver.com>
2020-01-16 16:27:41 +0900

    5. state 사용방법 익히기

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import Content from "./components/Content"
 import './App.css';
 
 class App extends Component {
+  constructor(props){ //가장 먼저 실행되어 state를 초기화한다.
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'world wide web!'}
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
