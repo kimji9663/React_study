1970906d88e9557aa41af3a183c708f40272b95a
kimji9663 <rlawlgp9663@naver.com>
2020-01-18 18:17:01 +0900

    9. Create, Update, Delete 기능 추가 : mode 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Subject from "./components/Subject"
 import TOC from "./components/TOC"
+import Control from './components/Control';
 import Content from "./components/Content"
 import './App.css';
 
@@ -59,6 +60,11 @@ class App extends Component {
           data={this.state.contents}
         >
         </TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          })
+        }.bind(this)}></Control>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
