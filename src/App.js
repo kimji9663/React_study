190f17181f515671cefd350fd30d57fff89926f6
kimji9663 <rlawlgp9663@naver.com>
2020-01-21 13:30:22 +0900

    13. delete 구현 : 항목을 선택하고 delete 클릭 시 목록에서 삭제

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super(props);
     this.max_content_id = 3;
     this.state = {
-      mode:'read',
+      mode:'welcome',
       selected_content_id:2,
       subject:{title:'WEB', sub:'world wide web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -110,9 +110,28 @@ class App extends Component {
         >
         </TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode:_mode
+          if(_mode === 'delete'){
+            if(window.confirm('really?')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while (i < this.state.contents.length) {
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i,1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+              alert('deleted!')
+            }
+          } else {
+            this.setState({
+              mode:_mode
           })
+        }
         }.bind(this)}></Control>
         {this.getContent()}
       </div>
