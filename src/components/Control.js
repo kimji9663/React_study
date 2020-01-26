6d833990dc88fafeec06d3dbf5885051e59665e6
kimji9663 <rlawlgp9663@naver.com>
2020-01-27 00:55:20 +0900

    SCSS 적용 : 버튼 레이아웃 변경

--- src/components/Control.js
+++ src/components/Control.js
@@ -6,15 +6,15 @@ class Control extends Component {
     return (
       <div id="buttons">
         <ul>
-          <li><a href="/create" onClick={function(e){
+          <li class="create"><a href="/create" onClick={function(e){
             e.preventDefault();
             this.props.onChangeMode('create');
           }.bind(this)}>create</a></li>
-          <li><a href="/update" onClick={function(e){
+          <li class="update"><a href="/update" onClick={function(e){
             e.preventDefault();
             this.props.onChangeMode('update');
           }.bind(this)}>update</a></li>
-          <li><input type="button" value="delete" onClick={function(e){
+          <li class="delete"><input type="button" value="delete" onClick={function(e){
             e.preventDefault();
             this.props.onChangeMode('delete');
           }.bind(this)}></input></li>
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -15,7 +15,7 @@ class Subject extends Component {
             {this.props.title}
           </a>
         </h1>
-        {this.props.sub}
+        <p>{this.props.sub}</p>
       </header>
     );
   }
